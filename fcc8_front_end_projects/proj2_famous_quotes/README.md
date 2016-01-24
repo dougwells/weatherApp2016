c33cb63d4b0136876133256b578de526ec360702
dougwells <dougwells@gmail.com>
2016-01-24 16:04:50 -0700

    Project 2: Quotes - Done.

---
+++ fcc8_front_end_projects/proj2_famous_quotes/README.md
@@ -0,0 +1,4 @@
+# Quotes
+Famous quotes that encourage all of us to be the best version of ourselves ...
+
+##Free Code Camp - Front End - Basic Project 2
--- fcc8_front_end_projects/proj2_famous_quotes/js/main.js
+++ fcc8_front_end_projects/proj2_famous_quotes/js/main.js
@@ -1,30 +1,39 @@
-$(window).scroll(function(){
-    if($('.navbar').offset().top > 50){
-        $('.navbar-fixed-top').addClass('top-nav-collapse');
-    } else {
-        $('.navbar-fixed-top').removeClass('top-nav-collapse');
-    }
-    $('#navbar li a').blur();
-});
+$(document).ready(function() {
 
-
-
-$(document).ready(function () {
-  //hide hamburger menu once clicked ...
-  $(".navbar-nav li a").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
-  });
-
-  //Gives HTML page a "scrolling" effect when click on link in SPA.  "Scrolls" down.
-  //Must include following line in HTML file for following jquery code to work
-  //<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-  $(function(){
-      $('.page-scroll a').bind('click', function(){
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500,'easeInOutExpo');
-          event.preventDefault();
-      });
+  //Download new quote & image URL from json datafile stored in my dropbox
+  //Replaces existing quote and image on webpage
+  $("#newQuote").click(function(){
+    $.ajax({
+      url: 'https://dl.dropboxusercontent.com/s/p5zx9nv7qpyexnc/quotes.json',
+      dataType: 'json',
+      success: function(data) {
+        var i = Math.floor(Math.random() * (data.length));
+        var quote = data[i].quote;
+        var author = data[i].author;
+        var imageUrl = data[i].imageUrl;
+        var imageColor = data[i].imageColor;
+        var twitterPost = "Love this quote by "+author+": '"+quote+"'";
+        var twitterPost140 = twitterPost.length > 140 ? twitterPost.substring(0,137)+"..." : twitterPost;
+        var quoteURI = encodeURI(twitterPost140);
+        var twitterQueryString = "https://twitter.com/intent/tweet?text="+quoteURI;
+        $('#quote').text("'"+quote+"'");
+        $('#quote-image').attr("src", imageUrl);
+        $('#author').text('Author: '+author);
+        $("#shareTwitter").attr("href", twitterQueryString);
+        console.log(twitterPost);
+        console.log(twitterQueryString);
+        console.log(twitterPost140);
+        console.log(twitterPost140.length);
+      },
+      error: function(err) {
+        var quote = "This app is shit!  You have an error. Error Message: "+ err;
+        var author = "Steve Jobs"
+        var imageUrl = "https://dl.dropboxusercontent.com/s/5ris4qilrskw5mr/sucks.jpg"
+        $("#quote").text("'"+quote+"'");
+        $('#quote-image').attr("src", imageUrl);
+        $('#author').text('Author: '+author);
+        console.log(quote);
+      }
+    });
   });
 });
